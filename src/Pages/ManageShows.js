5b9ae2adab726b873efa7dae382a1b7f2f33b613
Spencer Miller <millerspenstar@gmail.com>
2018-04-19 18:12:52 -0600

    add ability to manage shows in state

--- src/Pages/ManageShows.js
+++ src/Pages/ManageShows.js
@@ -4,8 +4,17 @@ import Show from '../Show'
 export default class ManageShows extends Component {
     state = {
         show: {
-            name: ''
-        }
+            name: '',
+            rating: -1,
+            previewImage: '',
+        
+        },
+        shows: [
+            {
+                name: 'Kung Fu Panda',
+                rating: 2,
+                previewImage:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk6siUv3oGQm6iZcGJdS-vdooYKp3A8laiRS6HEkg8JJlIRdiA' }
+        ]
     }
 
     handleOnChange = (event) => {
@@ -15,7 +24,7 @@ export default class ManageShows extends Component {
             })
         } else if (event.target.id === "ratingInput") {
             this.setState({
-                newShowRating: event.target.value
+                newShowRating: Number(event.target.value)
             })
         } else if (event.target.id === "previewImageInput") {
             this.setState({
@@ -26,25 +35,67 @@ export default class ManageShows extends Component {
 
     handleOnClick = () => {
         this.setState((prev) => {
-            return {
-                show: {
+            const existingShows = prev.shows
+            existingShows.push(
+            {
                     name: prev.newShowName,
                     rating: prev.newShowRating,
                     previewImage: prev.newShowPreviewImage
+                })
+                return{
+                    shows:existingShows
                 }
             }
+        )
+    }
+
+    renderShows= () =>{
+            
+            const showComponents = [
+           
+                
+
+        ]
+
+        // for(const show of this.state.shows){
+        //     showComponents.push(
+        //         <Show key={0} name={show.name} rating={show.rating} previewImage={show.previewImage} />
+
+        //     )
+        // }
+        
+        // for (let i= 0; i< this.state.shows.length; i++) {
+        //     const show = this.state.shows[i];
+
+        //     showComponents.push(
+        //        <Show key={i} name={show.name} rating={show.rating} previewImage={show.previewImage} />
+                
+                
+        //     )
+        // }
+
+
+        // return(
+        //     showComponents
+
+        // )
+
+        return this.state.shows.map((show, i) => {
+            return(
+                <Show key={i} name={show.name} rating={show.rating} previewImage={show.previewImage} />
+            )
+
         })
     }
 
     render() {
-        console.log("STATE", this.state)
+        // console.log("STATE", this.state)
         return (
             <div>
                 <section className="viewAllShows">
                     <header><h1>All Shows</h1></header>
                     <div>
-                        <Show name={this.state.show.name} rating={this.state.show.rating} previewImage={this.state.show.previewImage} />
-                        {/* <Show name="Show Name" /> */}
+                        {this.renderShows()}
                     </div>
                 </section>
                 <section className="createShow">
--- src/Show.js
+++ src/Show.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 import ReactPropTypes from 'prop-types'
 
 export default class Show extends Component {
+    static propTypes ={
+        name: ReactPropTypes.string.isRequired,
+        rating: ReactPropTypes.number.isRequired,
+        previewImage: ReactPropTypes.string.isRequired
+
+    }
+
     render() {
         return (
             <article>
